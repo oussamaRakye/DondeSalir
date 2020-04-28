e9b0bb9c6729ebf2768b35e1e6523e1bb16ca95b
oussamarakye <oussamarakye@gmail.com>
2020-04-29 00:03:56 +0200

    Notification if over 1km Updates real position every given time

--- app/src/main/java/com/oussamaRakye/dndesalir/MapsActivity.java
+++ app/src/main/java/com/oussamaRakye/dndesalir/MapsActivity.java
@@ -1,20 +1,31 @@
 package com.oussamaRakye.dndesalir;
 
-import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.fragment.app.FragmentActivity;
-
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.drawable.BitmapDrawable;
 import android.location.Location;
 import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
 import android.os.Looper;
+import android.os.Message;
 import android.provider.Settings;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.fragment.app.FragmentActivity;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
@@ -26,11 +37,16 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.CircleOptions;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+
+import java.util.concurrent.TimeUnit;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
@@ -38,17 +54,24 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     int PERMISSION_ID = 44;
     FusedLocationProviderClient mFusedLocationClient;
     LatLng currentLoc;
+    CircleOptions circle;
+    static Context context;
+    static Float message ;
+    Marker currentMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        context = this;
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
         mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         getLastLocation();
+        addMarker();
+
     }
 
     private boolean checkPermissions() {
@@ -81,6 +104,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 // Granted. Start getting the location information
                 getLastLocation();
+                addMarker();
             }
         }
     }
@@ -94,12 +118,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                             @Override
                             public void onComplete(@NonNull Task<Location> task) {
                                 Location location = task.getResult();
-                                if (location == null) {
-                                    requestNewLocationData();
-                                } else {
+                                if (location != null) {
                                     currentLoc = new LatLng(location.getLatitude(), location.getLongitude());
-                                    addMarker(currentLoc);
+                                    addMarker();
+                                    if (currentMarker != null)
+                                        currentMarker.setPosition(currentLoc);
                                 }
+                                requestNewLocationData();
                             }
                         }
                 );
@@ -113,17 +138,29 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
-    private void addMarker(LatLng currentLoc) {
-        mMap.addMarker(new MarkerOptions().position(currentLoc).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
-        CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLoc, 15);
-        mMap.animateCamera(cameraUpdate);
-        mMap.addCircle(new CircleOptions()
-                .center(currentLoc)
-                .radius(1000)
-                .strokeWidth(3)
-                .strokeColor(0xFF0000FF)
-                .fillColor(0x300000FF));
+    private void addMarker() {
+        if (currentLoc != null && circle == null) {
+            int height = 75;
+            int width = 75;
+            Bitmap b = BitmapFactory.decodeResource(getResources(), R.drawable. marker2);
+            Bitmap smallMarker = Bitmap.createScaledBitmap(b, width, height, false);
+            BitmapDescriptor smallMarkerIcon = BitmapDescriptorFactory.fromBitmap(smallMarker);
+            currentMarker = mMap.addMarker(new MarkerOptions()
+                    .icon(smallMarkerIcon)
+                    .position(currentLoc)
+                    .title("Punto actual"));
+            mMap.addMarker(new MarkerOptions().position(currentLoc).title("Punto de inicio"));
+            mMap.moveCamera(CameraUpdateFactory.newLatLng(currentLoc));
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLoc, 15);
+            mMap.animateCamera(cameraUpdate);
+            circle = new CircleOptions()
+                    .center(currentLoc)
+                    .radius(1000)
+                    .strokeWidth(3)
+                    .strokeColor(0xFF0000FF)
+                    .fillColor(0x300000FF);
+            mMap.addCircle(circle);
+        }
     }
 
     @SuppressLint("MissingPermission")
@@ -142,13 +179,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         );
 
     }
-
+    
     private LocationCallback mLocationCallback = new LocationCallback() {
         @Override
         public void onLocationResult(LocationResult locationResult) {
             Location mLastLocation = locationResult.getLastLocation();
             currentLoc = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            addMarker(currentLoc);
+            addMarker();
         }
     };
 
@@ -164,5 +201,98 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        Thread thread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (true) {
+                    try {
+                        TimeUnit.SECONDS.sleep(20);
+                    } catch (InterruptedException e) {
+
+                    }
+                    getLastLocation();
+                    checkIfInBounds();
+                }
+            }
+        });
+        thread.start();
     }
+
+    private void checkIfInBounds() {
+        float[] distance = new float[2];
+        if (currentLoc != null) {
+            Location.distanceBetween(currentLoc.latitude, currentLoc.longitude, circle.getCenter().latitude, circle.getCenter().longitude, distance);
+            //message = Math.sqrt(Math.pow(distance[0], 2) + Math.pow(distance[1], 2)) + "";
+            message = distance[0];
+        }
+        h.sendEmptyMessage(0);
+    }
+
+
+
+    @SuppressLint("HandlerLeak")
+    Handler h = new Handler() {
+        public void handleMessage(Message msg){
+            if(msg.what == 0 ){
+                Toast.makeText(context, message + "", Toast.LENGTH_SHORT).show();
+                if(message>1000)
+                    createNotification("¡Cuidado! Has pasado el km", context);
+            }
+        }
+    };
+
+
+    private NotificationManager notifManager;
+    public void createNotification(String aMessage, Context context) {
+        final int NOTIFY_ID = 0; // ID of notification
+        String id = "a"; // default_channel_id
+        String title = "Atención"; // Default Channel
+        Intent intent;
+        PendingIntent pendingIntent;
+        NotificationCompat.Builder builder;
+        if (notifManager == null) {
+            notifManager = (NotificationManager)context.getSystemService(Context.NOTIFICATION_SERVICE);
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            int importance = NotificationManager.IMPORTANCE_HIGH;
+            NotificationChannel mChannel = notifManager.getNotificationChannel(id);
+            if (mChannel == null) {
+                mChannel = new NotificationChannel(id, title, importance);
+                mChannel.enableVibration(true);
+                mChannel.setVibrationPattern(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+                notifManager.createNotificationChannel(mChannel);
+            }
+            builder = new NotificationCompat.Builder(context, id);
+            intent = new Intent(context, MapsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            builder.setContentTitle(aMessage)                            // required
+                    .setSmallIcon(R.drawable.icon)   // required
+                    .setContentText(context.getString(R.string.app_name)) // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400});
+        }
+        else {
+            builder = new NotificationCompat.Builder(context, id);
+            intent = new Intent(context, MapsActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_SINGLE_TOP);
+            pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
+            builder.setContentTitle(aMessage)                            // required
+                    .setSmallIcon(android.R.drawable.ic_popup_reminder)   // required
+                    .setContentText(context.getString(R.string.app_name)) // required
+                    .setDefaults(Notification.DEFAULT_ALL)
+                    .setAutoCancel(true)
+                    .setContentIntent(pendingIntent)
+                    .setTicker(aMessage)
+                    .setVibrate(new long[]{100, 200, 300, 400, 500, 400, 300, 200, 400})
+                    .setPriority(Notification.PRIORITY_HIGH);
+        }
+        Notification notification = builder.build();
+        notifManager.notify(NOTIFY_ID, notification);
+    }
+
 }
